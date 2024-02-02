ceec4e75d6fed3db727d402b383cdf6d7dbd9fe3
Nikos Spiropoulos <spiropoulos94@gmail.com>
2024-02-02 19:16:03 +0200

    Theme Init

--- homecare_frontend/src/App.js
+++ homecare_frontend/src/App.js
@@ -1,11 +1,12 @@
 // import { Box } from '@mui/material';
 import ThemeRoutes from './routes';
+import ThemeCustomization from './themes';
 
 function App() {
   return (
-    <>
+    <ThemeCustomization>
       <ThemeRoutes />
-    </>
+    </ThemeCustomization>
   );
 }
 
