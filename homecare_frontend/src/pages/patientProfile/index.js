19e456003baf48cf65d30394cb33e1740331e799
Nikos Spiropoulos <spiropoulos94@gmail.com>
2024-02-06 14:47:20 +0200

    Navigate between patient list & profile

--- homecare_frontend/src/pages/patientProfile/index.js
+++ homecare_frontend/src/pages/patientProfile/index.js
@@ -1,24 +1,34 @@
 // material-ui
-import { Typography } from '@mui/material';
+import { Box, Button } from '@mui/material';
 
-// project import
-import MainCard from 'components/MainCard';
-import { useParams } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate, useParams } from 'react-router-dom';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const PatientProfile = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
-
+  console.log({ id });
   return (
-    <MainCard title={`Patient ${id} Profile`}>
-      <Typography variant="body2">
-        Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-        minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-        reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui
-        officiate descent molls anim id est labours.
-      </Typography>
-    </MainCard>
+    <>
+      <Box sx={{ width: '100%', display: 'flex', mb: (theme) => theme.spacing(2) }}>
+        <Button startIcon={<ArrowBackIcon />} sx={{ ml: 'auto' }} onClick={() => navigate('/patients')}>
+          All Patients
+        </Button>
+      </Box>
+
+      {/* {patientData && (
+        <MainCard title={`Patient ${id} Profile - ${patientData.first_name} ${patientData.last_name}`}>
+          <Typography variant="body2">
+            Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif
+            ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
+            reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa
+            qui officiate descent molls anim id est labours.
+          </Typography>
+        </MainCard>
+      )} */}
+    </>
   );
 };
 
